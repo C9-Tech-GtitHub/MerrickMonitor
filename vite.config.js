ed121c7d2eaa74cd775094e12e840b778f18494f
MavC9 <tech@calibrenine.com.au>
2025-12-02 08:28:52 +1100

    fix: Use loadEnv in vite.config.js to properly load VITE_GOOGLE_CLIENT_ID in CI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: "/",
+    define: {
+      "import.meta.env.VITE_GOOGLE_CLIENT_ID": JSON.stringify(
+        env.VITE_GOOGLE_CLIENT_ID,
+      ),
+    },
+  };
 });
